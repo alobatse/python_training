08868b758a59c33074e877ea46c6f9cf1e83b246
alobatse <30293903+alobatse@users.noreply.github.com>
2017-08-04 18:54:06 +0300

    Adding condition check before moving to page

--- fixture/application.py
+++ fixture/application.py
@@ -7,7 +7,7 @@ class Application:
     def __init__ (self):
         self.wd = WebDriver(capabilities={"marionette": False},
                             firefox_binary="C:/Program Files (x86)/Mozilla Firefox ESR/firefox.exe")
-        self.wd.implicitly_wait(5)
+        #self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
@@ -23,7 +23,8 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://law-7/addressbook/")
+        if not (wd.current_url.endswith("law-7/addressbook/")):
+            wd.get("http://law-7/addressbook/")
 
     def destroy(self):
         self.wd.quit()
--- fixture/contact.py
+++ fixture/contact.py
@@ -76,11 +76,13 @@ class ContactHelper:
 
     def return_to_contact_page(self):
         wd = self.app.wd
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+        if not (wd.current_url.endswith("/addressbook") and len(wd.find_elements_by_name("searchstring")) > 0):
+            wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
     def open_add_contact_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("add new").click()
+        if not (wd.current_url.endswith("/edit.php") and len(wd.find_elements_by_name("firstname")) > 0):
+            wd.find_element_by_link_text("add new").click()
 
     def count(self):
         wd = self.app.wd
