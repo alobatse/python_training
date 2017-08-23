e32272b89fc031244584883ec88ef4867a08b320
alobatse <30293903+alobatse@users.noreply.github.com>
2017-08-23 19:10:30 +0300

    Adding test checking all Contacts on Home page with db

--- test/check_db_connection.py
+++ test/check_db_connection.py
@@ -1,13 +1,21 @@
 #import mysql.connector
 import pymysql.cursors
+from fixture.db import DbFixture
 
 
 #connection = mysql.connector.connect(host="law-7", database="addressbook", user="root", password="")
-connection = pymysql.connect(host="law-7", database="addressbook", user="root", password="")
+#connection = pymysql.connect(host="law-7", database="addressbook", user="root", password="")
+db = DbFixture(host="law-7", name="addressbook", user="root", password="")
+
 try:
-    cursor = connection.cursor()
-    cursor.execute("select * from group_list")
-    for row in cursor.fetchall():
-        print(row)
+    #cursor = connection.cursor()
+    #cursor.execute("select * from group_list")
+    #for row in cursor.fetchall():
+    #    print(row)
+    groups = db.get_group_list()
+    for group in groups:
+        print (group)
+    print (str(len(groups)))
 finally:
-    connection.close
+    db.destroy()
+    #connection.close
--- test/test_check_contact_info.py
+++ test/test_check_contact_info.py
@@ -1,4 +1,5 @@
 import re
+from model.contact import Contact
 
 def test_phones_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
@@ -22,6 +23,19 @@ def test_check_contact_info(app):
     assert merge_address(contact_from_home_page.address) == merge_address(contact_info_from_edit_page.address)
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_info_from_edit_page)
     assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_info_from_edit_page)
+
+
+def test_all_contacts_info(app, db):
+    contacts_from_home_page = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
+    contacts_info_from_db = sorted(db.get_contact_list(), key=Contact.id_or_max)
+    for contact_from_home_page in contacts_from_home_page:
+        index = contacts_from_home_page.index(contact_from_home_page)
+        contact_info_from_db = contacts_info_from_db[index]
+        assert contact_from_home_page.firstname.strip() == contact_info_from_db.firstname.strip()
+        assert contact_from_home_page.lastname.strip() == contact_info_from_db.lastname.strip()
+        assert merge_address(contact_from_home_page.address) == merge_address(contact_info_from_db.address)
+        assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_info_from_db)
+        assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_info_from_db)
 
 
 def clear_phones(s):
